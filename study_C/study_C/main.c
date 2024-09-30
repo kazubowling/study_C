517bd52ce1d90ddecc5c918737dc78618da027a6
kazubowling <kazubowling300@gmail.com>
2024-09-30 15:07:36 +0900

    いろいろな書式指定

--- study_C/study_C/main.c
+++ study_C/study_C/main.c
@@ -2,6 +2,15 @@
 
 #include <stdio.h>
 
-main() {
+main() 
+{
+	/*Hello World!*/
 	printf("Hello\nWorld!\n");
+
+	/*printf()‚Ìg‚¢•û*/
+	printf("%d-%d‚Í%d‚Å‚·B\n", 3, 2, 3 - 2);
+
+	/*‚¢‚ë‚¢‚ë‚È‘®w’è*/
+	printf("%s %c %f \n", "6€5", '=', 1.2);
+
 }
