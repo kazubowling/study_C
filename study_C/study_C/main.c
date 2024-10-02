9626f5446546632f348943e35031be2909b3e040
kazubowling <kazubowling300@gmail.com>
2024-10-02 18:35:36 +0900

    文字型

--- study_C/study_C/main.c
+++ study_C/study_C/main.c
@@ -23,11 +23,15 @@ main()
 	printf("%d\n", a);*/
 
 	/*数値型*/
-	unsigned char age = 25;
+	/*unsigned char age = 25;
 	double height = 166.7;
 	float weight = 58.5;
 
 	printf("年齢：%d歳\n", age);
 	printf("身長：%fcm\n", height);
-	printf("体重：%fkg\n", weight);
+	printf("体重：%fkg\n", weight);*/
+
+	char a = 'A';
+	printf("%d\n", a);
+	printf("%c\n", a);
 }
