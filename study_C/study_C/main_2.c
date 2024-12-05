8d1cf33ea04f444e6b3136a40d6097119efb1019
kazubowling <kazubowling300@gmail.com>
2024-12-05 09:27:41 +0900

    戻り値を利用する

--- study_C/study_C/main_2.c
+++ study_C/study_C/main_2.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-void dispnum(int a)
+/* 関数呼び出しの基本 */
+/*void dispnum(int a)
 {
     printf("引数の値は%d\n", a);
 }
@@ -11,4 +12,20 @@ int main()
 
     dispnum(5);
     dispnum(x);
+}*/
+
+/* 戻り値を利用する */
+int addnum(int a, int b)
+{
+    int x;
+
+    x = a + b;
+    return x;
+}
+
+int main()
+{
+    int n;
+    n = addnum(2, 3);
+    printf("戻り値は%d\n", n);
 }
