22b439fcb3d73ed28b11981e06dd1c728e2975d0
kazubowling <kazubowling300@gmail.com>
2024-12-05 19:52:24 +0900

    ローカル変数とグローバル変数

--- study_C/study_C/main_2.c
+++ study_C/study_C/main_2.c
@@ -15,7 +15,7 @@ int main()
 }*/
 
 /* 戻り値を利用する */
-int addnum(int a, int b)
+/*int addnum(int a, int b)
 {
     int x;
 
@@ -28,4 +28,29 @@ int main()
     int n;
     n = addnum(2, 3);
     printf("戻り値は%d\n", n);
+}*/
+
+/* ローカル変数とグローバル変数 */
+int y;
+int z;
+
+void myfunc(int a)
+{
+    int z;
+    int x;
+    x = a;
+    y = a;
+    z = a;
+}
+
+int main()
+{
+    int x;
+    x = 10;
+    y = 10;
+    z = 10;
+
+    printf("x, y, zの値は%d, %d, %d\n", x, y, z);
+    myfunc(5);
+    printf("x, y, zの値は%d, %d, %d\n", x, y, z);
 }
